fa74c2c3d14380e25160a0a9e38f440cdd958b69
Scott Campbell <scott396campbell@gmail.com>
2025-12-28 17:36:29 -0500

    Initial commit: Business Eval iPhone app

---
+++ Business Eval/Business_EvalApp.swift
@@ -0,0 +1,32 @@
+//
+//  Business_EvalApp.swift
+//  Business Eval
+//
+//  Created by Scott Campbell on 12/28/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Business_EvalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
